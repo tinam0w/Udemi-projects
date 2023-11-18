b0dc31b2047e66a547810d23a93b33e68b852667
Hristina Petrova <tina0alex@gmail.com>
2023-11-18 09:50:31 +0200

    add JPA

---
+++ Grade Submission/src/main/java/com/ltp/gradesubmission/entity/Grade.java
@@ -0,0 +1,13 @@
+package com.ltp.gradesubmission.entity;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Grade {
+
+    private Long id;
+    private String score;
+}
---
+++ Grade Submission/src/main/java/com/ltp/gradesubmission/entity/Student.java
@@ -0,0 +1,26 @@
+package com.ltp.gradesubmission.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "student")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "birth_date", nullable = false)
+    private LocalDate birthDate;
+}
